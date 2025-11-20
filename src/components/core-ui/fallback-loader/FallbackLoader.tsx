69c5f67e0c9dfe4f31d4b9d64f672c30621cddbc
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2025-11-20 16:17:04 +0500

    Enhance FallbackLoader component by adding removeHeight prop for conditional height rendering. Update ListingDetailModal to utilize the new prop, improving loading state display for the verification checkbox.

--- src/components/core-ui/fallback-loader/FallbackLoader.tsx
+++ src/components/core-ui/fallback-loader/FallbackLoader.tsx
@@ -1,10 +1,16 @@
 import { Spin } from "antd"
 
+interface FallBackLoaderTypes {
+    isModal?: boolean,
+    size?: "default" | "large" | "small",
+    className?: string,
+    removeHeight?: boolean
+}
 
-const FallbackLoader = ({ isModal = false, size = "default", className = "" }: { isModal?: boolean, size?: "default" | "large" | "small", className?: string }) => {
+const FallbackLoader = ({ isModal = false, size = "default", className = "", removeHeight = false }: FallBackLoaderTypes) => {
     const modalClass = "absolute backdrop-blur-sm w-full h-full z-50 centered-xy flex justify-center items-center"
     return (
-        <div className={`${isModal ? modalClass : 'flex justify-center items-center h-32'} ${className}`}>
+        <div className={`${isModal ? modalClass : `flex justify-center items-center ${removeHeight ? "" : "h-32"}`} ${className}`}>
             <Spin size={size} />
         </div>
     )
