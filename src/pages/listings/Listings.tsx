17bbe4520529a8bc77ac69acb5298a7c250c4b0b
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2025-11-26 14:53:05 +0500

    Enhance Listings component to utilize URL search parameters for tab management. Implement logic to update the tab query based on the selected tab and current search parameters, improving navigation consistency.

--- src/pages/listings/Listings.tsx
+++ src/pages/listings/Listings.tsx
@@ -3,18 +3,33 @@ import { useHeaderProps } from 'components/core/use-header-props';
 import TabSwitcher from 'components/core-ui/tab-switcher/TabSwitcher';
 import ApprovedListings from './ApprovedListings';
 import AiFlagListings from './AiFlagListings';
+import { useSearchParams } from 'react-router-dom';
 
 
 function Listings() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const currentTab = searchParams.get('tab');
     const { setTitle } = useHeaderProps();
-    const [selectedTab, setSelectedTab] = useState(0);
-
+    const [selectedTab, setSelectedTab] = useState<number>(currentTab === "approved" ? 0 : 1);
     useEffect(() => setTitle("Listings"), [setTitle]);
 
     const tabs = [
         { label: 'Approved' },
         { label: 'AI Flagged Requests' },
     ];
+
+    useEffect(() => {
+        if (currentTab !== "approved" && currentTab !== "aiflagged") {
+            updateTabQuery(0);
+        }
+    }, [currentTab]);
+
+    const updateTabQuery = (tab: number) => {
+        const nextSearchParams = new URLSearchParams(searchParams);
+        nextSearchParams.set('tab', tab === 0 ? "approved" : "aiflagged");
+        nextSearchParams.set('page', String(1));
+        setSearchParams(nextSearchParams);
+    };
 
     const renderTabContent = () => {
         switch (selectedTab) {
@@ -31,7 +46,10 @@ function Listings() {
         <section>
             <TabSwitcher
                 selectedTab={selectedTab}
-                onSelectTab={setSelectedTab}
+                onSelectTab={(v: number) => {
+                    setSelectedTab(v);
+                    updateTabQuery(v);
+                }}
                 tabs={tabs}
             />
             {renderTabContent()}
